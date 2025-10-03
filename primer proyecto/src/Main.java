776a8b68097e49b9ca0f32842e8139f9cd7c4494
Rafa Sotolongo Piñero <rafa102201@gmail.com>
2025-10-03 14:16:59 +0200

    update

--- primer proyecto/src/Main.java
+++ primer proyecto/src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 
-    public static void main ()
+    public static void main (String[] args ){
 
-    public void nombreMetodo()]
+
+
+    public void nombreMetodo(){
+    }
+    }
 
 
 
