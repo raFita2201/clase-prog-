cdd0b0bc872b45daf4e7c87e2109012a377815dd
Rafa Sotolongo Piñero <rafa102201@gmail.com>
2025-10-03 14:40:36 +0200

    update

--- primer proyecto/src/Main.java
+++ primer proyecto/src/Main.java
@@ -1,8 +1,12 @@
+import java.awt.*;
+
+// TODO ME FALTA ESTO
 public class Main {
 
     public static void main (String[] args ) {
         System.out.println("Este es mi pimera linea en Java");
-        System.out.println("Este es mi segunda linea en Java");
+        System.out.println("Este es mi segunda linea en Java\n\n\n");
+        System.out.println("Hola mundo");
 
     }
 
