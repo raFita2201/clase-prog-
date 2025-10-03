20fdea91c0484aa49b333192ae7491b586a1e31b
Rafa Sotolongo Piñero <rafa102201@gmail.com>
2025-10-03 14:24:34 +0200

    update

--- primer proyecto/src/Main.java
+++ primer proyecto/src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
 
     public static void main (String[] args ) {
+        System.out.println("Este es mi pimera linea en Java");
+        System.out.println("Este es mi segunda linea en Java");
 
     }
 
     public void nombreMetodo(){
     }
-    
+
 
 
 
