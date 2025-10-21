4799f9bb319c07b96a7ef8cf24c845101cb2ead1
Rafa Sotolongo Piñero <rafa102201@gmail.com>
2025-10-21 17:07:09 +0200

    update

--- T2/untitled/src/Entrada.java
+++ T2/untitled/src/Entrada.java
@@ -1,6 +1,10 @@
     public class Entrada {
 
-        public static void main(String[] args) { }
+        public static void main(String[] args) {
+            Operadores operadoresVariable = new Operadores();
+            operadoresVariable.operadoresAritmeticos();
+
+        }
 
 
 }
