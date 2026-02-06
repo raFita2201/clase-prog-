9e513d0e42d0ff61a9be7497a4e5bebf5ebe759e
Rafa Sotolongo Piñero <rafa102201@gmail.com>
2026-02-06 15:59:24 +0100

    update
    clase de lambdas

--- T6/Excepciones/src/Operaciones.java
+++ T6/Excepciones/src/Operaciones.java
@@ -1,3 +1,4 @@
+import utils.ResultadoException;
 
 public class Operaciones {
 
@@ -10,9 +11,24 @@ public class Operaciones {
         int suma = op1 + op2;
         int multiplicacion = op1 * op2;
         int division = op1 / op2;
+        int resta ;
+
+        try {
+            if (op2>op1){
+                throw new ResultadoException("El sistema detecta una resta negativa"+(op1 - op2));
+            }
+            resta= op1 - op2;
+        }catch (ResultadoException e){
+            resta=0;
+        }
+
+                // =op1 - op2;
+        // en caso de tener una resta negativa, quiero un fallo. Creamos nuestra propia excepción.
+            //la solución será poner la resta a 0.
         System.out.println("Los resultado son");
         System.out.println("Suma " + suma);
         System.out.println("Multiplicacion " + multiplicacion);
         System.out.println("Division " + division);
+        System.out.println("Resta = " + resta);
     }
 }
